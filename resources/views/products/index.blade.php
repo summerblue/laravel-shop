b4752dc4e7993ec26bfe7ce274b663120998db49
leo108 <leo108@qq.com>
2018-06-11 17:59:08 +0800

    用户界面 - 商品筛选、排序

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,6 +6,21 @@
 <div class="col-lg-10 col-lg-offset-1">
 <div class="panel panel-default">
   <div class="panel-body">
+    <div class="row">
+      <form action="{{ route('products.index') }}" class="form-inline search-form">
+        <input type="text" class="form-control input-sm" name="search" placeholder="搜索">
+        <button class="btn btn-primary btn-sm">搜索</button>
+        <select name="order" class="form-control input-sm pull-right">
+          <option value="">排序方式</option>
+          <option value="price_asc">价格从低到高</option>
+          <option value="price_desc">价格从高到低</option>
+          <option value="sold_count_desc">销量从高到低</option>
+          <option value="sold_count_asc">销量从低到高</option>
+          <option value="rating_desc">评价从高到低</option>
+          <option value="rating_asc">评价从低到高</option>
+        </select>
+      </form>
+    </div>
     <div class="row products-list">
       @foreach($products as $product)
       <div class="col-xs-3 product-item">
@@ -23,9 +38,23 @@
       </div>
       @endforeach
     </div>
-    <div class="pull-right">{{ $products->render() }}</div>
+    <div class="pull-right">{{ $products->appends($filters)->render() }}</div>
   </div>
 </div>
 </div>
 </div>
+@endsection
+
+@section('scriptsAfterJs')
+  <script>
+    var filters = {!! json_encode($filters) !!};
+    $(document).ready(function () {
+      $('.search-form input[name=search]').val(filters.search);
+      $('.search-form select[name=order]').val(filters.order);
+
+      $('.search-form select[name=order]').on('change', function() {
+        $('.search-form').submit();
+      });
+    })
+  </script>
 @endsection
