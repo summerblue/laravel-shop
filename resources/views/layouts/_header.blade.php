981de61c48f4ef78ab7ac526bec79ba39c77d4b9
leo108 <leo108@qq.com>
2018-06-11 18:12:49 +0800

    用户界面 - 收藏商品列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -32,6 +32,9 @@
                                 <a href="{{ route('user_addresses.index') }}">收货地址</a>
                             </li>
                             <li>
+                                <a href="{{ route('products.favorites') }}">我的收藏</a>
+                            </li>
+                            <li>
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes();
 
@@ -19,5 +18,8 @@ Route::group(['middleware' => 'auth'], function() {
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     });
 });
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
