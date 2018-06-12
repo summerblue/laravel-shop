04fd5a00897387c9795f340e2709611f01283cc1
leo108 <leo108@qq.com>
2018-06-12 12:01:42 +0800

    后台管理 - 同意退款 - 支付宝退款

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -49,6 +49,7 @@
       <!-- 订单发货开始 -->
       <!-- 如果订单未发货，展示发货表单 -->
       @if($order->ship_status === \App\Models\Order::SHIP_STATUS_PENDING)
+      @if($order->refund_status !== \App\Models\Order::REFUND_STATUS_SUCCESS)
       <tr>
         <td colspan="4">
           <form action="{{ route('admin.orders.ship', [$order->id]) }}" method="post" class="form-inline">
@@ -76,6 +77,7 @@
           </form>
         </td>
       </tr>
+      @endif
       @else
       <!-- 否则展示物流公司和物流单号 -->
       <tr>
@@ -150,5 +152,40 @@ $(document).ready(function() {
       });
     });
   });
+
+  // 同意按钮的点击事件
+  $('#btn-refund-agree').click(function() {
+    swal({
+      title: '确认要将款项退还给用户？',
+      type: 'warning',
+      showCancelButton: true,
+      closeOnConfirm: false,
+      confirmButtonText: "确认",
+      cancelButtonText: "取消",
+    }, function(ret){
+      // 用户点击取消，不做任何操作
+      if (!ret) {
+        return;
+      }
+      $.ajax({
+        url: '{{ route('admin.orders.handle_refund', [$order->id]) }}',
+        type: 'POST',
+        data: JSON.stringify({
+          agree: true, // 代表同意退款
+          _token: LA.token,
+        }),
+        contentType: 'application/json',
+        success: function (data) {
+          swal({
+            title: '操作成功',
+            type: 'success'
+          }, function() {
+            location.reload();
+          });
+        }
+      });
+    });
+  });
+
 });
 </script>
