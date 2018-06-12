e76b010e01631926fdfbc11bf2734d3b84b9d0b5
leo108 <leo108@qq.com>
2018-06-12 10:20:21 +0800

    订单支付

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -67,6 +67,13 @@
             @endif
           </div>
         </div>
+        <!-- 支付按钮开始 -->
+        @if(!$order->paid_at && !$order->closed)
+        <div class="payment-buttons">
+          <a class="btn btn-primary btn-sm" href="{{ route('payment.alipay', ['order' => $order->id]) }}">支付宝支付</a>
+        </div>
+        @endif
+        <!-- 支付按钮结束 -->
       </div>
     </div>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,10 @@ Route::group(['middleware' => 'auth'], function() {
         Route::post('orders', 'OrdersController@store')->name('orders.store');
         Route::get('orders', 'OrdersController@index')->name('orders.index');
         Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+        Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+        Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     });
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
