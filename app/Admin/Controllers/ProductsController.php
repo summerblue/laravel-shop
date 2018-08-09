386586858407635e4095e36fb60f3ad4e2536f55
leo108 <leo108@qq.com>
2018-08-09 11:55:35 +0800

    fix database null

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -111,7 +111,7 @@ class ProductsController extends Controller
             });
             // 定义事件回调，当模型即将保存时会触发这个回调
             $form->saving(function (Form $form) {
-                $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price');
+                $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
             });
         });
     }
