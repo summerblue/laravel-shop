54bf26a10369ed243f269c3c0599b8086b153d32
leo108 <leo108@qq.com>
2018-09-06 16:39:37 +0800

    顶部导航加入商品类目树

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 当 Laravel 渲染 products.index 和 products.show 模板时，就会使用 CategoryTreeComposer 这个来注入类目树变量
+        // 同时 Laravel 还支持通配符，例如 products.* 即代表当渲染 products 目录下的模板时都执行这个 ViewComposer
+        \View::composer(['products.index', 'products.show'], \App\Http\ViewComposers\CategoryTreeComposer::class);
     }
 
     /**
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -13,6 +13,18 @@
         </div>
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <ul class="nav navbar-nav">
+              <!-- 顶部类目菜单开始 -->
+              <!-- 判断模板是否有 $categoryTree 变量 -->
+              @if(isset($categoryTree))
+                <li>
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">所有类目 <b class="caret"></b></a>
+                  <ul class="dropdown-menu multi-level">
+                    <!-- 遍历 $categoryTree 集合，将集合中的每一项以 $category 变量注入 layouts._category_item 模板中并渲染 -->
+                    @each('layouts._category_item', $categoryTree, 'category')
+                  </ul>
+                </li>
+            @endif
+            <!-- 顶部类目菜单结束 -->
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <!-- 登录注册链接开始 -->
