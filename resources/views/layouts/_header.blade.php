3eb5628db0455694186b2a0f229661818d86888c
leo108 <leo108@qq.com>
2018-09-06 21:39:01 +0800

    前台分期付款列表页

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -50,6 +50,9 @@
                                 <a href="{{ route('orders.index') }}">我的订单</a>
                             </li>
                             <li>
+                                <a href="{{ route('installments.index') }}">分期付款</a>
+                            </li>
+                            <li>
                                 <a href="{{ route('products.favorites') }}">我的收藏</a>
                             </li>
                             <li>
--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::group(['middleware' => 'auth'], function() {
         Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
         Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
         Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
+        Route::get('installments', 'InstallmentsController@index')->name('installments.index');
     });
 });
 
