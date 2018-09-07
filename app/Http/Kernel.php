348394518bcc4fb67c5be0e6cc9d164094fa8e8d
leo108 <leo108@qq.com>
2018-09-07 15:23:42 +0800

    随机拒绝

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,5 +58,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'email_verified' => \App\Http\Middleware\CheckIfEmailVerified::class,
+        'random_drop' => \App\Http\Middleware\RandomDropSeckillRequest::class,
     ];
 }
