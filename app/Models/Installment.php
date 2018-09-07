a4e9faf62a548c679b0bc6b0c2852c83cbd8c87f
leo108 <leo108@qq.com>
2018-09-07 11:33:23 +0800

    分期付款退款

--- app/Models/Installment.php
+++ app/Models/Installment.php
@@ -65,4 +65,22 @@ class Installment extends Model
 
         return false;
     }
+
+    public function refreshRefundStatus()
+    {
+        $allSuccess = true;
+        // 重新加载 items，保证与数据库中数据同步
+        $this->load(['items']);
+        foreach ($this->items as $item) {
+            if ($item->paid_at && $item->refund_status !== InstallmentItem::REFUND_STATUS_SUCCESS) {
+                $allSuccess = false;
+                break;
+            }
+        }
+        if ($allSuccess) {
+            $this->order->update([
+                'refund_status' => Order::REFUND_STATUS_SUCCESS,
+            ]);
+        }
+    }
 }
