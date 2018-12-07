0244eb87ef86c98ca323176a48d514a0c1f75427
ixingjue <826739558@qq.com>
2018-12-07 12:21:26 +0800

    Fixed `https://laravel-china.org/topics/16682?#reply73955`

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -35,5 +36,21 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * 重写 showLoginForm 方法
+     * @url https://laravel-china.org/topics/16682?#reply73955
+     * @param \App\Http\Requests\Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showLoginForm(Request $request)
+    {
+        if ($request->session()->has('url.intended')) {
+            if (strpos($request->session()->get('url.intended'), '/admin')) {
+                $request->session()->forget('url.intended');
+            }
+        }
+        return view('auth.login');
     }
 }
