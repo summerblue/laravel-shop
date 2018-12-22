845907c8dc85ec4a9e963457ac0670c74a8c054b
leo108 <leo108@qq.com>
2018-12-22 15:18:57 +0800

    商品加入购物车

---
+++ app/Http/Requests/AddCartRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\ProductSku;
+
+class AddCartRequest extends Request
+{
+    public function rules()
+    {
+        return [
+            'sku_id' => [
+                'required',
+                function ($attribute, $value, $fail) {
+                    if (!$sku = ProductSku::find($value)) {
+                        return $fail('该商品不存在');
+                    }
+                    if (!$sku->product->on_sale) {
+                        return $fail('该商品未上架');
+                    }
+                    if ($sku->stock === 0) {
+                        return $fail('该商品已售完');
+                    }
+                    if ($this->input('amount') > 0 && $sku->stock < $this->input('amount')) {
+                        return $fail('该商品库存不足');
+                    }
+                },
+            ],
+            'amount' => ['required', 'integer', 'min:1'],
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'amount' => '商品数量'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'sku_id.required' => '请选择商品'
+        ];
+    }
+}
---
+++ app/Models/CartItem.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    protected $fillable = ['amount'];
+    public $timestamps = false;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function productSku()
+    {
+        return $this->belongsTo(ProductSku::class);
+    }
+}
