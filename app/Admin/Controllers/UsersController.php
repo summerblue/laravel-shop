07235e4175e6b81137b6209d382ee67037a4d51f
leo108 <leo108@qq.com>
2018-12-22 12:20:26 +0800

    管理后台 - 用户列表

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class UsersController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('用户列表')
+            ->body($this->grid());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User);
+
+        // 创建一个列名为 ID 的列，内容是用户的 id 字段
+        $grid->id('ID');
+
+        // 创建一个列名为 用户名 的列，内容是用户的 name 字段。下面的 email() 和 created_at() 同理
+        $grid->name('用户名');
+
+        $grid->email('邮箱');
+
+        $grid->email_verified_at('已验证邮箱')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+
+        $grid->created_at('注册时间');
+
+        // 不在页面显示 `新建` 按钮，因为我们不需要在后台新建用户
+        $grid->disableCreateButton();
+
+        $grid->actions(function ($actions) {
+            // 不在每一行后面展示查看按钮
+            $actions->disableView();
+            // 不在每一行后面展示删除按钮
+            $actions->disableDelete();
+            // 不在每一行后面展示编辑按钮
+            $actions->disableEdit();
+        });
+
+        $grid->tools(function ($tools) {
+            // 禁用批量删除按钮
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
+
+        return $grid;
+    }
+}
