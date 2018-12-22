e6984bbf5dd6e860c2273d9d6f9a04246bd16f67
leo108 <leo108@qq.com>
2018-12-22 14:51:07 +0800

    用户界面 - 商品详情页

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -34,9 +34,15 @@
         <div class="col-3 product-item">
           <div class="product-content">
             <div class="top">
-              <div class="img"><img src="{{ $product->image_url }}" alt=""></div>
+              <div class="img">
+                <a href="{{ route('products.show', ['product' => $product->id]) }}">
+                  <img src="{{ $product->image_url }}" alt="">
+                </a>
+              </div>
               <div class="price"><b>￥</b>{{ $product->price }}</div>
-              <div class="title">{{ $product->title }}</div>
+              <div class="title">
+                <a href="{{ route('products.show', ['product' => $product->id]) }}">{{ $product->title }}</a>
+              </div>
             </div>
             <div class="bottom">
               <div class="sold_count">销量 <span>{{ $product->sold_count }}笔</span></div>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes(['verify' => true]);
 
