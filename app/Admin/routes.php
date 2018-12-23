a4c225550e0a5f66c0f66474d44f8719cc6ceb0b
leo108 <leo108@qq.com>
2018-12-23 17:28:56 +0800

    管理后台 - 订单发货

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -21,4 +21,5 @@ Route::group([
 
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 });
