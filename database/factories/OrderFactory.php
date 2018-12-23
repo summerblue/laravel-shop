95b31f5dcb1f7f809f9d9cf36fa65a63cddbb0df
leo108 <leo108@qq.com>
2018-12-23 14:23:17 +0800

    订单模型

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Order::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/OrderItemFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\OrderItem::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
