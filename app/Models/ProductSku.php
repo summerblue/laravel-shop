ae5898ab8d31b0c6720e5aaebaf877b370ab5da4
leo108 <leo108@qq.com>
2018-12-23 15:04:04 +0800

    创建订单

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Exceptions\InternalException;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductSku extends Model
@@ -11,5 +12,22 @@ class ProductSku extends Model
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可小于0');
+        }
+
+        return $this->newQuery()->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可小于0');
+        }
+        $this->increment('stock', $amount);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,6 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('cart', 'CartController@add')->name('cart.add');
     Route::get('cart', 'CartController@index')->name('cart.index');
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+    Route::post('orders', 'OrdersController@store')->name('orders.store');
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
