8f7af32e438d2d55a334c7b0321af675bffe92e3
leo108 <leo108@qq.com>
2018-12-23 18:54:21 +0800

    管理后台 - 新增、修改、删除优惠券

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -25,4 +25,9 @@ Route::group([
     $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');
 
     $router->get('coupon_codes', 'CouponCodesController@index');
+    $router->post('coupon_codes', 'CouponCodesController@store');
+    $router->get('coupon_codes/create', 'CouponCodesController@create');
+    $router->get('coupon_codes/{id}/edit', 'CouponCodesController@edit');
+    $router->put('coupon_codes/{id}', 'CouponCodesController@update');
+    $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
 });
