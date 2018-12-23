e107356a27c87021cb08a8e582c74b17d000a355
leo108 <leo108@qq.com>
2018-12-23 20:00:36 +0800

    优化优惠券模块

--- app/Jobs/CloseOrder.php
+++ app/Jobs/CloseOrder.php
@@ -40,6 +40,9 @@ class CloseOrder implements ShouldQueue
             foreach ($this->order->items as $item) {
                 $item->productSku->addStock($item->amount);
             }
+            if ($this->order->couponCode) {
+                $this->order->couponCode->changeUsed(false);
+            }
         });
     }
 }
