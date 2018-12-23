3541520b37b995fe98007e8dad3da20cf51f9abb
leo108 <leo108@qq.com>
2018-12-23 16:42:25 +0800

    引入支付库

---
+++ config/pay.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'alipay' => [
+        'app_id'         => '',
+        'ali_public_key' => '',
+        'private_key'    => '',
+        'log'            => [
+            'file' => storage_path('logs/alipay.log'),
+        ],
+    ],
+
+    'wechat' => [
+        'app_id'      => '',
+        'mch_id'      => '',
+        'key'         => '',
+        'cert_client' => '',
+        'cert_key'    => '',
+        'log'         => [
+            'file' => storage_path('logs/wechat_pay.log'),
+        ],
+    ],
+];
