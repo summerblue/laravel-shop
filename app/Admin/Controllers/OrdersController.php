107a6f10d4979dfb45148eff7fcfcd89f2a13c40
leo108 <leo108@qq.com>
2018-12-23 17:59:13 +0800

    管理后台 - 拒绝退款

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Exceptions\InvalidRequestException;
+use App\Http\Requests\Admin\HandleRefundRequest;
 use App\Models\Order;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\HasResourceActions;
@@ -92,5 +93,29 @@ class OrdersController extends Controller
 
         // 返回上一页
         return redirect()->back();
+    }
+
+    public function handleRefund(Order $order, HandleRefundRequest $request)
+    {
+        // 判断订单状态是否正确
+        if ($order->refund_status !== Order::REFUND_STATUS_APPLIED) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+        // 是否同意退款
+        if ($request->input('agree')) {
+            // 同意退款的逻辑这里先留空
+            // todo
+        } else {
+            // 将拒绝退款理由放到订单的 extra 字段中
+            $extra = $order->extra ?: [];
+            $extra['refund_disagree_reason'] = $request->input('reason');
+            // 将订单的退款状态改为未退款
+            $order->update([
+                'refund_status' => Order::REFUND_STATUS_PENDING,
+                'extra'         => $extra,
+            ]);
+        }
+
+        return $order;
     }
 }
--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -89,6 +89,12 @@
               未支付
             @endif
           </div>
+          @if(isset($order->extra['refund_disagree_reason']))
+            <div>
+              <span>拒绝退款理由：</span>
+              <div class="value">{{ $order->extra['refund_disagree_reason'] }}</div>
+            </div>
+          @endif
         </div>
         <!-- 支付按钮开始 -->
         @if(!$order->paid_at && !$order->closed)
