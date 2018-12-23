ea0370fa9524dde62dc4d1c8ce44011cae446b50
leo108 <leo108@qq.com>
2018-12-23 17:00:49 +0800

    订单支付

--- routes/web.php
+++ routes/web.php
@@ -34,5 +34,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('orders', 'OrdersController@store')->name('orders.store');
     Route::get('orders', 'OrdersController@index')->name('orders.index');
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+    Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+    Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
