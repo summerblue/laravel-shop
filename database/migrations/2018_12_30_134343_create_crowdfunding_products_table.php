68b4ad815f80f372bfecaf018fd5f9b3e6f47980
leo108 <leo108@qq.com>
2018-12-31 15:34:37 +0800

    初始化众筹模型

---
+++ database/migrations/2018_12_30_134343_create_crowdfunding_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCrowdfundingProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('crowdfunding_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->decimal('target_amount', 10, 2);
+            $table->decimal('total_amount', 10, 2)->default(0);
+            $table->unsignedInteger('user_count')->default(0);
+            $table->dateTime('end_at');
+            $table->string('status')->default(\App\Models\CrowdfundingProduct::STATUS_FUNDING);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('crowdfunding_products');
+    }
+}
