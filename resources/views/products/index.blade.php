120c553b77c002b004fad5081064476d73e2e71e
leo108 <leo108@qq.com>
2018-12-31 15:13:08 +0800

    前台商品列表支持按类目筛选

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,6 +11,27 @@
       <div class="form-row">
         <div class="col-md-9">
           <div class="form-row">
+            <!-- 面包屑开始 -->
+            <div class="col-auto category-breadcrumb">
+              <!-- 添加一个名为 全部 的链接，直接跳转到商品列表页 -->
+              <a class="all-products" href="{{ route('products.index') }}">全部</a> >
+              <!-- 如果当前是通过类目筛选的 -->
+              @if ($category)
+              <!-- 遍历这个类目的所有祖先类目，我们在模型的访问器中已经排好序，因此可以直接使用 -->
+              @foreach($category->ancestors as $ancestor)
+                <!-- 添加一个名为该祖先类目名的链接 -->
+                <span class="category">
+                  <a href="{{ route('products.index', ['category_id' => $ancestor->id]) }}">{{ $ancestor->name }}</a>
+                 </span>
+                <span>&gt;</span>
+              @endforeach
+              <!-- 最后展示出当前类目名称 -->
+                <span class="category">{{ $category->name }}</span><span> ></span>
+                <!-- 当前类目的 ID，当用户调整排序方式时，可以保证 category_id 参数不丢失 -->
+                <input type="hidden" name="category_id" value="{{ $category->id }}">
+              @endif
+            </div>
+            <!-- 面包屑结束 -->
             <div class="col-auto"><input type="text" class="form-control form-control-sm" name="search" placeholder="搜索"></div>
             <div class="col-auto"><button class="btn btn-primary btn-sm">搜索</button></div>
           </div>
@@ -29,6 +50,22 @@
       </div>
     </form>
     <!-- 筛选组件结束 -->
+    <!-- 展示子类目开始 -->
+    <div class="filters">
+      <!-- 如果当前是通过类目筛选，并且此类目是一个父类目 -->
+      @if ($category && $category->is_directory)
+        <div class="row">
+          <div class="col-3 filter-key">子类目：</div>
+          <div class="col-9 filter-values">
+            <!-- 遍历直接子类目 -->
+            @foreach($category->children as $child)
+              <a href="{{ route('products.index', ['category_id' => $child->id]) }}">{{ $child->name }}</a>
+            @endforeach
+          </div>
+        </div>
+      @endif
+    </div>
+    <!-- 展示子类目结束 -->
     <div class="row products-list">
       @foreach($products as $product)
         <div class="col-3 product-item">
