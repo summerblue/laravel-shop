442632ff900668da3b63233fb77fd13150f80f34
leo108 <leo108@qq.com>
2019-01-01 19:34:13 +0800

    开发环境加入 Ngrok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,7 +32,7 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
-            $config['notify_url'] = route('payment.alipay.notify');
+            $config['notify_url'] = ngrok_url('payment.alipay.notify');
             $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
@@ -47,7 +47,7 @@ class AppServiceProvider extends ServiceProvider
 
         $this->app->singleton('wechat_pay', function () {
             $config = config('pay.wechat');
-            $config['notify_url'] = route('payment.wechat.notify');
+            $config['notify_url'] = ngrok_url('payment.wechat.notify');
             if (app()->environment() !== 'production') {
                 $config['log']['level'] = Logger::DEBUG;
             } else {
--- config/app.php
+++ config/app.php
@@ -228,4 +228,5 @@ return [
 
     'order_ttl' => 1800,
 
+    'ngrok_url' => env('NGROK_URL'),
 ];
