03a4920b47ede538a09426954e112c67c182f18c
leo108 <leo108@qq.com>
2019-01-02 22:59:50 +0800

    创建分期付款

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -110,6 +110,12 @@
             <a class="btn btn-primary btn-sm" href="{{ route('payment.alipay', ['order' => $order->id]) }}">支付宝支付</a>
             <!-- 把之前的微信支付按钮换成这个 -->
             <button class="btn btn-sm btn-success" id='btn-wechat'>微信支付</button>
+            <!-- 分期支付按钮开始 -->
+            <!-- 仅当订单总金额大等于分期最低金额时才展示分期按钮 -->
+            @if ($order->total_amount >= config('app.min_installment_amount'))
+              <button class="btn btn-sm btn-danger" id='btn-installment'>分期付款</button>
+            @endif
+            <!-- 分期支付按钮结束 -->
           </div>
         @endif
         <!-- 支付按钮结束 -->
@@ -133,6 +139,45 @@
 </div>
 </div>
 </div>
+<!-- 分期弹框开始 -->
+<div class="modal fade" id="installment-modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">选择分期期数</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <table class="table table-bordered table-striped text-center">
+          <thead>
+          <tr>
+            <th class="text-center">期数</th>
+            <th class="text-center">费率</th>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          @foreach(config('app.installment_fee_rate') as $count => $rate)
+            <tr>
+              <td>{{ $count }}期</td>
+              <td>{{ $rate }}%</td>
+              <td>
+                <button class="btn btn-sm btn-primary btn-select-installment" data-count="{{ $count }}">选择</button>
+              </td>
+            </tr>
+          @endforeach
+          </tbody>
+        </table>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- 分期弹框结束 -->
 @endsection
 
 @section('scriptsAfterJs')
@@ -195,6 +240,21 @@
               });
             });
         });
+      });
+      // 分期付款按钮点击事件
+      $('#btn-installment').click(function () {
+        // 展示分期弹框
+        $('#installment-modal').modal();
+      });
+
+      // 选择分期期数按钮点击事件
+      $('.btn-select-installment').click(function () {
+        // 调用创建分期付款接口
+        axios.post('{{ route('payment.installment', ['order' => $order->id]) }}', { count: $(this).data('count') })
+          .then(function (response) {
+            console.log(response.data);
+            // todo 跳转到分期付款页面
+          })
       });
 
     });
--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
+    Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
 
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 
