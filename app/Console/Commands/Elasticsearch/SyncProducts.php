af5f325d2faded100be43572bbf10ab5ed5759b9
leo108 <leo108@qq.com>
2019-01-05 11:34:14 +0800

    Elasticsearch 索引迁移命令

--- app/Console/Commands/Elasticsearch/SyncProducts.php
+++ app/Console/Commands/Elasticsearch/SyncProducts.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 
 class SyncProducts extends Command
 {
-    protected $signature = 'es:sync-products';
+    protected $signature = 'es:sync-products {--index=products}';
 
     protected $description = '将商品数据同步到 Elasticsearch';
 
@@ -36,7 +36,8 @@ class SyncProducts extends Command
 
                     $req['body'][] = [
                         'index' => [
-                            '_index' => 'products',
+                            // 从参数中读取索引名称
+                            '_index' => $this->option('index'),
                             '_type'  => '_doc',
                             '_id'    => $data['id'],
                         ],
