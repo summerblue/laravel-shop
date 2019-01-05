3d7590c80ca1b8d49beb75212d9433b2f7045064
leo108 <leo108@qq.com>
2019-01-05 16:42:15 +0800

    减少秒杀接口 SQL 查询

--- app/Http/Requests/SeckillOrderRequest.php
+++ app/Http/Requests/SeckillOrderRequest.php
@@ -1,9 +1,11 @@
 <?php
 namespace App\Http\Requests;
 
+use App\Exceptions\InvalidRequestException;
 use App\Models\Order;
 use App\Models\Product;
 use App\Models\ProductSku;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\Rule;
 
 class SeckillOrderRequest extends Request
@@ -18,26 +20,34 @@ class SeckillOrderRequest extends Request
             'address.zip'           => 'required',
             'address.contact_name'  => 'required',
             'address.contact_phone' => 'required',
-            'sku_id'     => [
+            'sku_id'                => [
                 'required',
                 function ($attribute, $value, $fail) {
-                    if (!$sku = ProductSku::find($value)) {
+                    // 从 Redis 中读取数据
+                    $stock = \Redis::get('seckill_sku_'.$value);
+                    // 如果是 null 代表这个 SKU 不是秒杀商品
+                    if (is_null($stock)) {
                         return $fail('该商品不存在');
                     }
-                    if ($sku->product->type !== Product::TYPE_SECKILL) {
-                        return $fail('该商品不支持秒杀');
+                    // 判断库存
+                    if ($stock < 1) {
+                        return $fail('该商品已售完');
                     }
+
+                    // 大多数用户在上面的逻辑里就被拒绝了
+                    // 因此下方的 SQL 查询不会对整体性能有太大影响
+                    $sku = ProductSku::find($value);
                     if ($sku->product->seckill->is_before_start) {
                         return $fail('秒杀尚未开始');
                     }
                     if ($sku->product->seckill->is_after_end) {
                         return $fail('秒杀已经结束');
                     }
-                    if (!$sku->product->on_sale) {
-                        return $fail('该商品未上架');
+                    if (!$user = \Auth::user()) {
+                        throw new AuthenticationException('请先登录');
                     }
-                    if ($sku->stock < 1) {
-                        return $fail('该商品已售完');
+                    if (!$user->email_verified) {
+                        throw new InvalidRequestException('请先验证邮箱');
                     }
 
                     if ($order = Order::query()
