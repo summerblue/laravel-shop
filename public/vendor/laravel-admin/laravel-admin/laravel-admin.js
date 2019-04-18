4f514eeca422e6aa730c7f7273b9580417a8383e
leo108 <root@leo108.com>
2019-04-18 23:51:20 +0800

    初始化管理后台

---
+++ public/vendor/laravel-admin/laravel-admin/laravel-admin.js
@@ -0,0 +1,87 @@
+$.fn.editable.defaults.params = function (params) {
+    params._token = LA.token;
+    params._editable = 1;
+    params._method = 'PUT';
+    return params;
+};
+
+$.fn.editable.defaults.error = function (data) {
+    var msg = '';
+    if (data.responseJSON.errors) {
+        $.each(data.responseJSON.errors, function (k, v) {
+            msg += v + "\n";
+        });
+    }
+    return msg
+};
+
+toastr.options = {
+    closeButton: true,
+    progressBar: true,
+    showMethod: 'slideDown',
+    timeOut: 4000
+};
+
+$.pjax.defaults.timeout = 5000;
+$.pjax.defaults.maxCacheLength = 0;
+$(document).pjax('a:not(a[target="_blank"])', {
+    container: '#pjax-container'
+});
+
+NProgress.configure({parent: '#app'});
+
+$(document).on('pjax:timeout', function (event) {
+    event.preventDefault();
+})
+
+$(document).on('submit', 'form[pjax-container]', function (event) {
+    $.pjax.submit(event, '#pjax-container')
+});
+
+$(document).on("pjax:popstate", function () {
+
+    $(document).one("pjax:end", function (event) {
+        $(event.target).find("script[data-exec-on-popstate]").each(function () {
+            $.globalEval(this.text || this.textContent || this.innerHTML || '');
+        });
+    });
+});
+
+$(document).on('pjax:send', function (xhr) {
+    if (xhr.relatedTarget && xhr.relatedTarget.tagName && xhr.relatedTarget.tagName.toLowerCase() === 'form') {
+        $submit_btn = $('form[pjax-container] :submit');
+        if ($submit_btn) {
+            $submit_btn.button('loading')
+        }
+    }
+    NProgress.start();
+});
+
+$(document).on('pjax:complete', function (xhr) {
+    if (xhr.relatedTarget && xhr.relatedTarget.tagName && xhr.relatedTarget.tagName.toLowerCase() === 'form') {
+        $submit_btn = $('form[pjax-container] :submit');
+        if ($submit_btn) {
+            $submit_btn.button('reset')
+        }
+    }
+    NProgress.done();
+});
+
+$(function () {
+    $('.sidebar-menu li:not(.treeview) > a').on('click', function () {
+        var $parent = $(this).parent().addClass('active');
+        $parent.siblings('.treeview.active').find('> a').trigger('click');
+        $parent.siblings().removeClass('active').find('li').removeClass('active');
+    });
+    var menu = $('.sidebar-menu li > a[href="' + (location.pathname + location.search + location.hash) + '"]').parent().addClass('active');
+    menu.parents('ul.treeview-menu').addClass('menu-open');
+    menu.parents('li.treeview').addClass('active');
+
+    $('[data-toggle="popover"]').popover();
+});
+
+(function ($) {
+    $.fn.admin = LA;
+    $.admin = LA;
+
+})(jQuery);
