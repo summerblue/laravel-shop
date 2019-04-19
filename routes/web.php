f9a1bc438f40541cd3d0d02b1ff90ab4b4765f85
leo108 <root@leo108.com>
2019-04-19 22:39:38 +0800

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+
+    Route::get('products', 'ProductsController@index')->name('products.index');
+    Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 });
