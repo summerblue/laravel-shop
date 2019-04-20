06d6db36922f53c3134b85de093cb909a943c2d3
leo108 <root@leo108.com>
2019-04-20 15:59:36 +0800

    用户界面 - 申请退款

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
     Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
     Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
+    Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
 
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
