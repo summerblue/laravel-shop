60a69b8f7383a3e0406e41bc699ef6521338ac18
leo108 <root@leo108.com>
2019-04-20 08:52:35 +0800

    创建订单

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -50,6 +50,33 @@
             @endforeach
             </tbody>
           </table>
+          <!-- 开始 -->
+          <div>
+            <form class="form-horizontal" role="form" id="order-form">
+              <div class="form-group row">
+                <label class="col-form-label col-sm-3 text-md-right">选择收货地址</label>
+                <div class="col-sm-9 col-md-7">
+                  <select class="form-control" name="address">
+                    @foreach($addresses as $address)
+                      <option value="{{ $address->id }}">{{ $address->full_address }} {{ $address->contact_name }} {{ $address->contact_phone }}</option>
+                    @endforeach
+                  </select>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-form-label col-sm-3 text-md-right">备注</label>
+                <div class="col-sm-9 col-md-7">
+                  <textarea name="remark" class="form-control" rows="3"></textarea>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="offset-sm-3 col-sm-3">
+                  <button type="button" class="btn btn-primary btn-create-order">提交订单</button>
+                </div>
+              </div>
+            </form>
+          </div>
+          <!-- 结束 -->
         </div>
       </div>
     </div>
@@ -94,6 +121,55 @@
           // 将其勾选状态设为与目标单选框一致
           $(this).prop('checked', checked);
         });
+      });
+
+      // 监听创建订单按钮的点击事件
+      $('.btn-create-order').click(function () {
+        // 构建请求参数，将用户选择的地址的 id 和备注内容写入请求参数
+        var req = {
+          address_id: $('#order-form').find('select[name=address]').val(),
+          items: [],
+          remark: $('#order-form').find('textarea[name=remark]').val(),
+        };
+        // 遍历 <table> 标签内所有带有 data-id 属性的 <tr> 标签，也就是每一个购物车中的商品 SKU
+        $('table tr[data-id]').each(function () {
+          // 获取当前行的单选框
+          var $checkbox = $(this).find('input[name=select][type=checkbox]');
+          // 如果单选框被禁用或者没有被选中则跳过
+          if ($checkbox.prop('disabled') || !$checkbox.prop('checked')) {
+            return;
+          }
+          // 获取当前行中数量输入框
+          var $input = $(this).find('input[name=amount]');
+          // 如果用户将数量设为 0 或者不是一个数字，则也跳过
+          if ($input.val() == 0 || isNaN($input.val())) {
+            return;
+          }
+          // 把 SKU id 和数量存入请求参数数组中
+          req.items.push({
+            sku_id: $(this).data('id'),
+            amount: $input.val(),
+          })
+        });
+        axios.post('{{ route('orders.store') }}', req)
+          .then(function (response) {
+            swal('订单提交成功', '', 'success');
+          }, function (error) {
+            if (error.response.status === 422) {
+              // http 状态码为 422 代表用户输入校验失败
+              var html = '<div>';
+              _.each(error.response.data.errors, function (errors) {
+                _.each(errors, function (error) {
+                  html += error+'<br>';
+                })
+              });
+              html += '</div>';
+              swal({content: $(html)[0], icon: 'error'})
+            } else {
+              // 其他情况应该是系统挂了
+              swal('系统错误', '', 'error');
+            }
+          });
       });
 
     });
