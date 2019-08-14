76cbed4368abb4d622eeb946780f4746bd3aeb27
周奕奇 <137968399@qq.com>
2019-08-14 13:47:49 +0800

    fixbug

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,16 +7,16 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     $image = $faker->randomElement([
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/7kG1HekGK6.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/1B3n0ATKrn.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/r3BNRe4zXG.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/C0bVuKB2nt.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/82Wf2sg8gM.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/nIvBAQO5Pj.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/XrtIwzrxj7.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/uYEHCJ1oRp.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/2JMRaFwRpo.jpg",
-        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/pa7DrV43Mw.jpg",
+        "https://cdn.learnku.com/uploads/images/201806/01/5320/7kG1HekGK6.jpg",
+        "https://cdn.learnku.com/uploads/images/201806/01/5320/1B3n0ATKrn.jpg",
+        "https://cdn.learnku.com/uploads/images/201806/01/5320/r3BNRe4zXG.jpg",
+        "https://cdn.learnku.com/uploads/images/201806/01/5320/C0bVuKB2nt.jpg",
+        "https://cdn.learnku.com/uploads/images/201806/01/5320/82Wf2sg8gM.jpg",
+        "https://cdn.learnku.com/uploads/images/201806/01/5320/nIvBAQO5Pj.jpg",
+        "https://cdn.learnku.com/uploads/images/201806/01/5320/XrtIwzrxj7.jpg",
+        "https://cdn.learnku.com/uploads/images/201806/01/5320/uYEHCJ1oRp.jpg",
+        "https://cdn.learnku.com/uploads/images/201806/01/5320/2JMRaFwRpo.jpg",
+        "https://cdn.learnku.com/uploads/images/201806/01/5320/pa7DrV43Mw.jpg",
     ]);
 
     return [
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css')
-  .version();
+    .sass('resources/sass/app.scss', 'public/css')
+    .version();
+
