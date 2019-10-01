bb63c1a07a79015a4017ac0ddf828ddcbe9d2572
leo108 <root@leo108.com>
2019-10-01 09:18:10 +0800

    邮箱验证功能

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
