787a4afdaa8bc02882185118c1330536eb55072a
leo108 <root@leo108.com>
2019-10-01 09:39:53 +0800

    新增收货地址

---
+++ app/Http/Requests/UserAddressRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+class UserAddressRequest extends Request
+{
+    public function rules()
+    {
+        return [
+            'province'      => 'required',
+            'city'          => 'required',
+            'district'      => 'required',
+            'address'       => 'required',
+            'zip'           => 'required',
+            'contact_name'  => 'required',
+            'contact_phone' => 'required',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'province'      => '省',
+            'city'          => '城市',
+            'district'      => '地区',
+            'address'       => '详细地址',
+            'zip'           => '邮编',
+            'contact_name'  => '姓名',
+            'contact_phone' => '电话',
+        ];
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -16,10 +16,8 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
