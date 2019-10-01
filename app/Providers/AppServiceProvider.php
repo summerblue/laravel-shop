145b5ccc939ce92d852832b23bda6bc54ff31d50
leo108 <root@leo108.com>
2019-10-01 17:22:42 +0800

    微信支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,6 +33,7 @@ class AppServiceProvider extends ServiceProvider
 
         $this->app->singleton('wechat_pay', function () {
             $config = config('pay.wechat');
+            $config['notify_url'] = route('payment.wechat.notify');
             if (app()->environment() !== 'production') {
                 $config['log']['level'] = Logger::DEBUG;
             } else {
