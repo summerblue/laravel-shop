10c10f2909943ee29454a203f82c1feaba18a544
leo108 <root@leo108.com>
2019-10-01 10:47:18 +0800

    用户界面 - 商品列表

--- database/factories/ProductSkuFactory.php
+++ database/factories/ProductSkuFactory.php
@@ -1,12 +1,13 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-
 use App\Models\ProductSku;
 use Faker\Generator as Faker;
 
 $factory->define(ProductSku::class, function (Faker $faker) {
     return [
-        //
+        'title'       => $faker->word,
+        'description' => $faker->sentence,
+        'price'       => $faker->randomNumber(4),
+        'stock'       => $faker->randomNumber(5),
     ];
 });
