abf5b3e876148c1f1c509af5704c17f71cec2912
leo108 <root@leo108.com>
2019-10-01 16:43:27 +0800

    订单模型

---
+++ database/factories/OrderItemFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\OrderItem;
+use Faker\Generator as Faker;
+
+$factory->define(OrderItem::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
