f1b82c1e618b012cc8f0b96c6fae3ef5c0e98217
leo108 <root@leo108.com>
2019-10-01 17:15:57 +0800

    订单支付

---
+++ resources/views/pages/success.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('title', '操作成功')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">操作成功</div>
+        <div class="card-body text-center">
+            <h1>{{ $msg }}</h1>
+            <a class="btn btn-primary" href="{{ route('root') }}">返回首页</a>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,11 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('orders', 'OrdersController@store')->name('orders.store');
     Route::get('orders', 'OrdersController@index')->name('orders.index');
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+
+    Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+    Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
 });
+
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
