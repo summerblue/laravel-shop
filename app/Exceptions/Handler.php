b590d76565202abaf0b548caaeffb26b2062f7ce
leo108 <root@leo108.com>
2019-10-05 17:57:26 +0800

    用户界面 - 使用优惠券下单

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -14,6 +14,7 @@ class Handler extends ExceptionHandler
      */
     protected $dontReport = [
         InvalidRequestException::class,
+        CouponCodeUnavailableException::class,
     ];
 
     /**
