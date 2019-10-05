58b49d97a2ebcff51db2097495cc4139031ef586
leo108 <root@leo108.com>
2019-10-05 17:34:25 +0800

    管理后台 - 同意退款 - 微信支付退款

--- routes/web.php
+++ routes/web.php
@@ -47,5 +47,6 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
+Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
