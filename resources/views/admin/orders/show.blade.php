7f256f79dfa92bb66d170c12d14a92a2885a9776
leo108 <root@leo108.com>
2019-10-05 17:10:25 +0800

    管理后台 - 订单发货

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -41,8 +41,51 @@
             @endforeach
             <tr>
                 <td>订单金额：</td>
-                <td colspan="3">￥{{ $order->total_amount }}</td>
+                <td>￥{{ $order->total_amount }}</td>
+                <!-- 这里也新增了一个发货状态 -->
+                <td>发货状态：</td>
+                <td>{{ \App\Models\Order::$shipStatusMap[$order->ship_status] }}</td>
             </tr>
+            <!-- 订单发货开始 -->
+            <!-- 如果订单未发货，展示发货表单 -->
+            @if($order->ship_status === \App\Models\Order::SHIP_STATUS_PENDING)
+                <tr>
+                    <td colspan="4">
+                        <form action="{{ route('admin.orders.ship', [$order->id]) }}" method="post" class="form-inline">
+                            <!-- 别忘了 csrf token 字段 -->
+                            {{ csrf_field() }}
+                            <div class="form-group {{ $errors->has('express_company') ? 'has-error' : '' }}">
+                                <label for="express_company" class="control-label">物流公司</label>
+                                <input type="text" id="express_company" name="express_company" value="" class="form-control" placeholder="输入物流公司">
+                                @if($errors->has('express_company'))
+                                    @foreach($errors->get('express_company') as $msg)
+                                        <span class="help-block">{{ $msg }}</span>
+                                    @endforeach
+                                @endif
+                            </div>
+                            <div class="form-group {{ $errors->has('express_no') ? 'has-error' : '' }}">
+                                <label for="express_no" class="control-label">物流单号</label>
+                                <input type="text" id="express_no" name="express_no" value="" class="form-control" placeholder="输入物流单号">
+                                @if($errors->has('express_no'))
+                                    @foreach($errors->get('express_no') as $msg)
+                                        <span class="help-block">{{ $msg }}</span>
+                                    @endforeach
+                                @endif
+                            </div>
+                            <button type="submit" class="btn btn-success" id="ship-btn">发货</button>
+                        </form>
+                    </td>
+                </tr>
+            @else
+                <!-- 否则展示物流公司和物流单号 -->
+                <tr>
+                    <td>物流公司：</td>
+                    <td>{{ $order->ship_data['express_company'] }}</td>
+                    <td>物流单号：</td>
+                    <td>{{ $order->ship_data['express_no'] }}</td>
+                </tr>
+            @endif
+            <!-- 订单发货结束 -->
             </tbody>
         </table>
     </div>
