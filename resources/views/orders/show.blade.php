b1d670ddf75d109702613d14c3cd68e9d94742a9
leo108 <root@leo108.com>
2019-11-02 17:30:17 +0800

    前台分期付款详情页

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -253,8 +253,7 @@
           // 调用创建分期付款接口
           axios.post('{{ route('payment.installment', ['order' => $order->id]) }}', { count: $(this).data('count') })
             .then(function (response) {
-              console.log(response.data);
-              // todo 跳转到分期付款页面
+              location.href = '/installments/' + response.data.id;
             })
         });
 
