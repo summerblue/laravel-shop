28f541ecb175bce2b491aace7994eba916b5878d
leo108 <root@leo108.com>
2019-11-02 17:04:49 +0800

    订单模块调整

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,6 +7,9 @@ use Ramsey\Uuid\Uuid;
 
 class Order extends Model
 {
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+
     const REFUND_STATUS_PENDING = 'pending';
     const REFUND_STATUS_APPLIED = 'applied';
     const REFUND_STATUS_PROCESSING = 'processing';
@@ -16,6 +19,11 @@ class Order extends Model
     const SHIP_STATUS_PENDING = 'pending';
     const SHIP_STATUS_DELIVERED = 'delivered';
     const SHIP_STATUS_RECEIVED = 'received';
+
+    public static $typeMap = [
+        self::TYPE_NORMAL => '普通商品订单',
+        self::TYPE_CROWDFUNDING => '众筹商品订单',
+    ];
 
     public static $refundStatusMap = [
         self::REFUND_STATUS_PENDING    => '未退款',
@@ -32,6 +40,7 @@ class Order extends Model
     ];
 
     protected $fillable = [
+        'type',
         'no',
         'address',
         'total_amount',
--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -119,8 +119,10 @@
                                     <button type="button" id="btn-receive" class="btn btn-sm btn-success">确认收货</button>
                                 </div>
                             @endif
-                            <!-- 订单已支付，且退款状态是未退款时展示申请退款按钮 -->
-                            @if($order->paid_at && $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
+                            <!-- 不是众筹订单，已支付，且退款状态是未退款时展示申请退款按钮 -->
+                            @if($order->type !== \App\Models\Order::TYPE_CROWDFUNDING &&
+                                $order->paid_at &&
+                                $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
                                 <div class="refund-button">
                                     <button class="btn btn-sm btn-danger" id="btn-apply-refund">申请退款</button>
                                 </div>
