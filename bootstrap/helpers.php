c26fa8eb4cbe8cd9aa0aa3d69a4683e46032fe03
leo108 <root@leo108.com>
2019-11-02 17:22:09 +0800

    初始化分期付款模型

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -14,3 +14,9 @@ function ngrok_url($routeName, $parameters = [])
 
     return route($routeName, $parameters);
 }
+
+// 默认的精度为小数点后两位
+function big_number($number, $scale = 2)
+{
+    return new \Moontoast\Math\BigNumber($number, $scale);
+}
