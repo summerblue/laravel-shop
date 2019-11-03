4873c22014f8c4a85ba4b5c917cbb41fbbc70e21
leo108 <root@leo108.com>
2019-11-03 19:22:26 +0800

    随机拒绝

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'random_drop' => \App\Http\Middleware\RandomDropSeckillRequest::class,
     ];
 
     /**
