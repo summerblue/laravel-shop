38cc0f42bac3c85d7d1d01ea5a6ff3841e863613
leo108 <root@leo108.com>
2020-05-04 11:16:18 +0800

    注册与登录

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+
+Auth::routes();
