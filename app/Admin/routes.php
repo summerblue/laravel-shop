b0db3f5a52f1763dc0aea7fb7115dd730f03f828
leo108 <root@leo108.com>
2020-05-05 16:29:59 +0800

    管理后台 - 商品列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,4 +12,5 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->get('users', 'UsersController@index');
+    $router->get('products', 'ProductsController@index');
 });
