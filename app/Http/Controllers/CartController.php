68b33813f47b5f17332e407deed67a518e0191f5
leo108 <root@leo108.com>
2020-05-05 17:35:07 +0800

    创建订单

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -12,8 +12,9 @@ class CartController extends Controller
     public function index(Request $request)
     {
         $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
 
-        return view('cart.index', ['cartItems' => $cartItems]);
+        return view('cart.index', ['cartItems' => $cartItems, 'addresses' => $addresses]);
     }
 
     public function add(AddCartRequest $request)
--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('cart', 'CartController@index')->name('cart.index');
     Route::post('cart', 'CartController@add')->name('cart.add');
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+    Route::post('orders', 'OrdersController@store')->name('orders.store');
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
