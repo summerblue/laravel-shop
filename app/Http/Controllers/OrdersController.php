14b54a8895526a9379519fb401ca257fcad894d3
leo108 <root@leo108.com>
2020-05-05 17:39:09 +0800

    关闭未支付订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Jobs\CloseOrder;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
 use App\Models\Order;
@@ -63,6 +64,8 @@ class OrdersController extends Controller
 
             return $order;
         });
+
+        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
         return $order;
     }
---
+++ app/Jobs/CloseOrder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Models\Order;
+
+// 代表这个类需要被放到队列中执行，而不是触发时立即执行
+class CloseOrder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $order;
+
+    public function __construct(Order $order, $delay)
+    {
+        $this->order = $order;
+        // 设置延迟的时间，delay() 方法的参数代表多少秒之后执行
+        $this->delay($delay);
+    }
+
+    // 定义这个任务类具体的执行逻辑
+    // 当队列处理器从队列中取出任务时，会调用 handle() 方法
+    public function handle()
+    {
+        // 判断对应的订单是否已经被支付
+        // 如果已经支付则不需要关闭订单，直接退出
+        if ($this->order->paid_at) {
+            return;
+        }
+        // 通过事务执行 sql
+        \DB::transaction(function() {
+            // 将订单的 closed 字段标记为 true，即关闭订单
+            $this->order->update(['closed' => true]);
+            // 循环遍历订单中的商品 SKU，将订单中的数量加回到 SKU 的库存中去
+            foreach ($this->order->items as $item) {
+                $item->productSku->addStock($item->amount);
+            }
+        });
+    }
+}
