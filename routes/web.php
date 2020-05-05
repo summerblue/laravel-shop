4ecba3d4d6a160496d50c092bcdd2e2141508c3d
leo108 <root@leo108.com>
2020-05-05 16:48:21 +0800

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 Auth::routes(['verify' => true]);
 
