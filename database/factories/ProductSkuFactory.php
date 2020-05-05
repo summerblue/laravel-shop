c971482c1ac45fe29f7a71765cae21ada4324655
leo108 <root@leo108.com>
2020-05-05 16:27:54 +0800

    商品模型

---
+++ database/factories/ProductSkuFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\ProductSku;
+use Faker\Generator as Faker;
+
+$factory->define(ProductSku::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
