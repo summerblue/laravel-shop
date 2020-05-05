f9d9818f6e0958dae3c786b3ecc1ee12778971d2
leo108 <root@leo108.com>
2020-05-05 17:10:05 +0800

    用户界面 - 商品收藏

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,11 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(UserAddress::class);
     }
+
+    public function favoriteProducts()
+    {
+        return $this->belongsToMany(Product::class, 'user_favorite_products')
+            ->withTimestamps()
+            ->orderBy('user_favorite_products.created_at', 'desc');
+    }
 }
