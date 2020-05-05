1f6326aeb25a73ec4ae820dea491cf2665f1cafb
leo108 <root@leo108.com>
2020-05-05 18:52:40 +0800

    管理后台 - 订单详情

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -5,6 +5,7 @@ namespace App\Admin\Controllers;
 use App\Models\Order;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
 
 class OrdersController extends AdminController
 {
@@ -43,5 +44,13 @@ class OrdersController extends AdminController
         });
 
         return $grid;
+    }
+
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('查看订单')
+            // body 方法可以接受 Laravel 的视图作为参数
+            ->body(view('admin.orders.show', ['order' => Order::find($id)]));
     }
 }
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -18,4 +18,5 @@ Route::group([
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
 });
