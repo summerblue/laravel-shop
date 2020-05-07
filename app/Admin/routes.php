3a0ea8dab92baaf43be49c036d39193fd0f4e59c
leo108 <root@leo108.com>
2020-05-07 22:03:56 +0800

    管理后台 - 订单发货

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -19,4 +19,5 @@ Route::group([
     $router->put('products/{id}', 'ProductsController@update');
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 });
