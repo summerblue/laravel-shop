66d4a3bc4a4c84f5c7b4cebfd50047675f9338cf
leo108 <root@leo108.com>
2020-05-07 22:25:28 +0800

    管理后台 - 拒绝退款

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -20,4 +20,5 @@ Route::group([
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
+    $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');
 });
