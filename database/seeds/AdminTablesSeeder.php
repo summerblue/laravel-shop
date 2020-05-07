ff3765e3d6ec14a2dec9d25ef9229978c22a5604
leo108 <root@leo108.com>
2020-05-07 23:48:22 +0800

    导出管理后台数据

---
+++ database/seeds/AdminTablesSeeder.php
@@ -0,0 +1,234 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // base tables
+        Encore\Admin\Auth\Database\Menu::truncate();
+        Encore\Admin\Auth\Database\Menu::insert(
+            [
+                [
+                    "parent_id" => 0,
+                    "order" => 1,
+                    "title" => "首页",
+                    "icon" => "fa-bar-chart",
+                    "uri" => "/",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 6,
+                    "title" => "系统管理",
+                    "icon" => "fa-tasks",
+                    "uri" => NULL,
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 7,
+                    "title" => "管理员",
+                    "icon" => "fa-users",
+                    "uri" => "auth/users",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 8,
+                    "title" => "角色",
+                    "icon" => "fa-user",
+                    "uri" => "auth/roles",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 9,
+                    "title" => "权限",
+                    "icon" => "fa-ban",
+                    "uri" => "auth/permissions",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 10,
+                    "title" => "菜单",
+                    "icon" => "fa-bars",
+                    "uri" => "auth/menu",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 11,
+                    "title" => "操作日志",
+                    "icon" => "fa-history",
+                    "uri" => "auth/logs",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 2,
+                    "title" => "用户管理",
+                    "icon" => "fa-users",
+                    "uri" => "/users",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 3,
+                    "title" => "商品管理",
+                    "icon" => "fa-cubes",
+                    "uri" => "/products",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 4,
+                    "title" => "订单管理",
+                    "icon" => "fa-cny",
+                    "uri" => "/orders",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 5,
+                    "title" => "优惠券管理",
+                    "icon" => "fa-tags",
+                    "uri" => "/coupon_codes",
+                    "permission" => NULL
+                ]
+            ]
+        );
+
+        Encore\Admin\Auth\Database\Permission::truncate();
+        Encore\Admin\Auth\Database\Permission::insert(
+            [
+                [
+                    "name" => "All permission",
+                    "slug" => "*",
+                    "http_method" => "",
+                    "http_path" => "*"
+                ],
+                [
+                    "name" => "Dashboard",
+                    "slug" => "dashboard",
+                    "http_method" => "GET",
+                    "http_path" => "/"
+                ],
+                [
+                    "name" => "Login",
+                    "slug" => "auth.login",
+                    "http_method" => "",
+                    "http_path" => "/auth/login\r\n/auth/logout"
+                ],
+                [
+                    "name" => "User setting",
+                    "slug" => "auth.setting",
+                    "http_method" => "GET,PUT",
+                    "http_path" => "/auth/setting"
+                ],
+                [
+                    "name" => "Auth management",
+                    "slug" => "auth.management",
+                    "http_method" => "",
+                    "http_path" => "/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs"
+                ],
+                [
+                    "name" => "用户管理",
+                    "slug" => "users",
+                    "http_method" => "",
+                    "http_path" => "/users*"
+                ],
+                [
+                    "name" => "商品管理",
+                    "slug" => "products",
+                    "http_method" => "",
+                    "http_path" => "/products*"
+                ],
+                [
+                    "name" => "优惠券管理",
+                    "slug" => "coupon_codes",
+                    "http_method" => "",
+                    "http_path" => "/coupon_codes*"
+                ],
+                [
+                    "name" => "订单管理",
+                    "slug" => "orders",
+                    "http_method" => "",
+                    "http_path" => "/orders*"
+                ]
+            ]
+        );
+
+        Encore\Admin\Auth\Database\Role::truncate();
+        Encore\Admin\Auth\Database\Role::insert(
+            [
+                [
+                    "name" => "Administrator",
+                    "slug" => "administrator"
+                ],
+                [
+                    "name" => "运营",
+                    "slug" => "operation"
+                ]
+            ]
+        );
+
+        // pivot tables
+        DB::table('admin_role_menu')->truncate();
+        DB::table('admin_role_menu')->insert(
+            [
+                [
+                    "role_id" => 1,
+                    "menu_id" => 2
+                ]
+            ]
+        );
+
+        DB::table('admin_role_permissions')->truncate();
+        DB::table('admin_role_permissions')->insert(
+            [
+                [
+                    "role_id" => 1,
+                    "permission_id" => 1
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 2
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 3
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 4
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 6
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 7
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 8
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 9
+                ]
+            ]
+        );
+
+        // finish
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(AdminTablesSeeder::class);
         $this->call(UsersSeeder::class);
         $this->call(UserAddressesSeeder::class);
         $this->call(ProductsSeeder::class);
