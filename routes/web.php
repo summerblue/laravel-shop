02172286c70ceedd0841b4523ddaced036718cad
leo108 <root@leo108.com>
2020-08-01 16:57:00 +0800

    前台分期付款列表页

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 
     Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
+
+    Route::get('installments', 'InstallmentsController@index')->name('installments.index');
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
