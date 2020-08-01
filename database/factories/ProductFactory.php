7663133c3e8d05bdff06d43faa4694f19d995d05
leo108 <root@leo108.com>
2020-08-01 15:02:05 +0800

    商品后台添加类目字段

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -18,6 +18,8 @@ $factory->define(Product::class, function (Faker $faker) {
         "https://cdn.learnku.com/uploads/images/201806/01/5320/2JMRaFwRpo.jpg",
         "https://cdn.learnku.com/uploads/images/201806/01/5320/pa7DrV43Mw.jpg",
     ]);
+    // 从数据库中随机取一个类目
+    $category = \App\Models\Category::query()->where('is_directory', false)->inRandomOrder()->first();
 
     return [
         'title'        => $faker->word,
@@ -28,5 +30,8 @@ $factory->define(Product::class, function (Faker $faker) {
         'sold_count'   => 0,
         'review_count' => 0,
         'price'        => 0,
+        // 将取出的类目 ID 赋给 category_id 字段
+        // 如果数据库中没有类目则 $category 为 null，同样 category_id 也设成 null
+        'category_id'  => $category ? $category->id : null,
     ];
 });
