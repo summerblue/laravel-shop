2b1dcccfb018dae0c8fe7ea5edd37b4b4a47ef87
leo108 <root@leo108.com>
2020-08-01 14:49:36 +0800

    类目 Seeder 文件

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminTablesSeeder::class);
         $this->call(UsersSeeder::class);
         $this->call(UserAddressesSeeder::class);
+        // 放在 ProductsSeeder 之前
+        $this->call(CategoriesSeeder::class);
         $this->call(ProductsSeeder::class);
         $this->call(CouponCodesSeeder::class);
         $this->call(OrdersSeeder::class);
