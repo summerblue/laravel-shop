4d66d08f566a06252e9d9a91f05b11650e262008
leo108 <root@leo108.com>
2020-08-01 16:55:23 +0800

    创建分期付款

--- config/app.php
+++ config/app.php
@@ -232,4 +232,12 @@ return [
     'order_ttl' => 1800,
 
     'ngrok_url' => env('NGROK_URL'),
+
+    'installment_fee_rate' => [
+        3  => 1.5,
+        6  => 2,
+        12 => 2.5,
+    ], // 分期费率，key 为期数，value 为费率
+    'min_installment_amount' => 300, // 最低分期金额
+    'installment_fine_rate' => 0.05, // 逾期日息 0.05%
 ];
