0969b58f5640d001b232877578c554a1efd53f39
leo108 <root@leo108.com>
2020-08-02 09:16:14 +0800

    秒杀商品详情页

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -19,10 +19,12 @@ class Order extends Model
 
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
+    const TYPE_SECKILL = 'seckill';
 
     public static $typeMap = [
         self::TYPE_NORMAL => '普通商品订单',
         self::TYPE_CROWDFUNDING => '众筹商品订单',
+        self::TYPE_SECKILL => '秒杀商品订单',
     ];
 
     public static $refundStatusMap = [
