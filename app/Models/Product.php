807c133672fcd6b9512aee6cba0184b38c1df9ae
leo108 <root@leo108.com>
2020-08-02 09:02:23 +0800

    初始化秒杀模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,9 +10,11 @@ class Product extends Model
 {
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
+    const TYPE_SECKILL = 'seckill';
     public static $typeMap = [
         self::TYPE_NORMAL  => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
+        self::TYPE_SECKILL => '秒杀商品',
     ];
 
     protected $fillable = [
@@ -46,6 +48,11 @@ class Product extends Model
     public function crowdfunding()
     {
         return $this->hasOne(CrowdfundingProduct::class);
+    }
+
+    public function seckill()
+    {
+        return $this->hasOne(SeckillProduct::class);
     }
 
     public function properties()
