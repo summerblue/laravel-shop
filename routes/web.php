f34f8c709cd39d7090aabd3c0c4f6f9e0cc595e3
Summer <summer@learnku.com>
2021-03-09 08:26:49 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
