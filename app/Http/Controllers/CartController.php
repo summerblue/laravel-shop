d738d099d08375f443de3d44d8a34a88b74e0f52
Summer <summer@learnku.com>
2021-03-09 09:16:46 +0000

    封装业务代码

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,47 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\AddCartRequest;
-use App\Models\CartItem;
 use App\Models\ProductSku;
+use App\Services\CartService;
 
 class CartController extends Controller
 {
-    public function add(AddCartRequest $request)
+    protected $cartService;
+
+    // 利用 Laravel 的自动解析功能注入 CartService 类
+    public function __construct(CartService $cartService)
     {
-        $user   = $request->user();
-        $skuId  = $request->input('sku_id');
-        $amount = $request->input('amount');
-
-        // 从数据库中查询该商品是否已经在购物车中
-        if ($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
-
-            // 如果存在则直接叠加商品数量
-            $cart->update([
-                'amount' => $cart->amount + $amount,
-            ]);
-        } else {
-
-            // 否则创建一个新的购物车记录
-            $cart = new CartItem(['amount' => $amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
-
-        return [];
+        $this->cartService = $cartService;
     }
 
     public function index(Request $request)
     {
-        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $cartItems = $this->cartService->get();
         $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
 
         return view('cart.index', ['cartItems' => $cartItems, 'addresses' => $addresses]);
     }
 
+    public function add(AddCartRequest $request)
+    {
+        $this->cartService->add($request->input('sku_id'), $request->input('amount'));
+
+        return [];
+    }
+
     public function remove(ProductSku $sku, Request $request)
     {
-        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+        $this->cartService->remove($sku->id);
 
         return [];
     }
