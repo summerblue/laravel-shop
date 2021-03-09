721d3ca807375f2b561f3d418607b1636273f345
Summer <summer@learnku.com>
2021-03-09 09:36:25 +0000

    集成支付宝

--- config/pay.php
+++ config/pay.php
@@ -2,20 +2,20 @@
 
 return [
     'alipay' => [
-        'app_id'         => '',
-        'ali_public_key' => '',
-        'private_key'    => '',
+        'app_id'         => env('ALIPAY_APP_ID'),
+        'ali_public_key' => env('ALIPAY_PUBLIC_KEY'),
+        'private_key'    => env('ALIPAY_PRIVATE_KEY'),
         'log'            => [
             'file' => storage_path('logs/alipay.log'),
         ],
     ],
 
     'wechat' => [
-        'app_id'      => '',
-        'mch_id'      => '',
-        'key'         => '',
-        'cert_client' => '',
-        'cert_key'    => '',
+        'app_id'      => env('WECHAT_PAY_APP_ID'),
+        'mch_id'      => env('WECHAT_PAY_MCH_ID'),
+        'key'         => env('WECHAT_PAY_KEY'),
+        'cert_client' => resource_path('wechat_pay/apiclient_cert.pem'),
+        'cert_key'    => resource_path('wechat_pay/apiclient_key.pem'),
         'log'         => [
             'file' => storage_path('logs/wechat_pay.log'),
         ],
