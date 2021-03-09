0cb99776243b056ff7ec49cfe1ed61ada29d63de
Summer <summer@learnku.com>
2021-03-09 12:20:53 +0000

    管理后台 - 修改、删除优惠券

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -79,7 +79,14 @@ class CouponCodesController extends AdminController
 
         $form->display('id', 'ID');
         $form->text('name', '名称')->rules('required');
-        $form->text('code', '优惠码')->rules('nullable|unique:coupon_codes');
+        $form->text('code', '优惠码')->rules(function($form) {
+            // 如果 $form->model()->id 不为空，代表是编辑操作
+            if ($id = $form->model()->id) {
+                return 'nullable|unique:coupon_codes,code,'.$id.',id';
+            } else {
+                return 'nullable|unique:coupon_codes';
+            }
+        });
         $form->radio('type', '类型')->options(CouponCode::$typeMap)->rules('required')->default(CouponCode::TYPE_FIXED);
         $form->text('value', '折扣')->rules(function ($form) {
             if (request()->input('type') === CouponCode::TYPE_PERCENT) {
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -24,5 +24,8 @@ Route::group([
     $router->get('coupon_codes', 'CouponCodesController@index');
     $router->post('coupon_codes', 'CouponCodesController@store');
     $router->get('coupon_codes/create', 'CouponCodesController@create');
+    $router->get('coupon_codes/{id}/edit', 'CouponCodesController@edit');
+    $router->put('coupon_codes/{id}', 'CouponCodesController@update');
+    $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
 
 });
