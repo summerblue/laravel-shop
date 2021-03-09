2af7d515f7455836a2a71499d258dd4e68f07ee5
Summer <summer@learnku.com>
2021-03-09 12:22:36 +0000

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
     Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
     Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 });
 
 Route::redirect('/', '/products')->name('root');
