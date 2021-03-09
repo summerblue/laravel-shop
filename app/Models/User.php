84cb0477878776b9000b3adf03ac4590e58cf1d0
Summer <summer@learnku.com>
2021-03-09 00:36:07 +0000

    邮箱验证功能

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
+
+// 在之前的路由里加上一个 verify 参数
+Auth::routes(['verify' => true]);
