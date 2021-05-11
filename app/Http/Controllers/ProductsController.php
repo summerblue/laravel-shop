84e07f2e7e810ae874a01eaffae488f570ef6cd3
Summer <summer@learnku.com>
2021-05-11 10:28:54 +0000

    前台商品列表支持按类目筛选

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Exceptions\InvalidRequestException;
 use App\Models\OrderItem;
+use App\Models\Category;
 
 class ProductsController extends Controller
 {
@@ -13,8 +14,6 @@ class ProductsController extends Controller
     {
         // 创建一个查询构造器
         $builder = Product::query()->where('on_sale', true);
-        // 判断是否有提交 search 参数，如果有就赋值给 $search 变量
-        // search 参数用来模糊搜索商品
         if ($search = $request->input('search', '')) {
             $like = '%'.$search.'%';
             // 模糊搜索商品标题、商品详情、SKU 标题、SKU描述
@@ -26,6 +25,21 @@ class ProductsController extends Controller
                             ->orWhere('description', 'like', $like);
                     });
             });
+        }
+
+        // 如果有传入 category_id 字段，并且在数据库中有对应的类目
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            // 如果这是一个父类目
+            if ($category->is_directory) {
+                // 则筛选出该父类目下所有子类目的商品
+                $builder->whereHas('category', function ($query) use ($category) {
+                    // 这里的逻辑参考本章第一节
+                    $query->where('path', 'like', $category->path.$category->id.'-%');
+                });
+            } else {
+                // 如果这不是一个父类目，则直接筛选此类目下的商品
+                $builder->where('category_id', $category->id);
+            }
         }
 
         // 是否有提交 order 参数，如果有就赋值给 $order 变量
@@ -49,6 +63,7 @@ class ProductsController extends Controller
                 'search' => $search,
                 'order'  => $order,
             ],
+            'category' => $category ?? null,
         ]);
     }
 
